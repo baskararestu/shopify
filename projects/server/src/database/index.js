dd2309ef6cc185f1263726dd345a4fd81e7359d7
Abdi Alghifara Felinanda <52093497+Abdi-01@users.noreply.github.com>
2023-07-26 11:13:48 +0700

    Update index.js

--- projects/server/src/database/index.js
+++ projects/server/src/database/index.js
@@ -1,16 +1,14 @@
-require("dotenv").config({
-  path: ".env.local",
-});
+const { join } = require("path");
+require("dotenv").config({ path: join(__dirname, "../../.env") });
 const mysql = require("mysql2");
 const util = require("util");
-const env = process.env;
 
 const db = mysql.createConnection({
-  host: env.MYSQL_HOST,
-  user: env.MYSQL_USER,
-  password: env.MYSQL_PASSWORD,
-  database: env.MYSQL_DB_NAME,
-  port: env.MYSQL_PORT,
+  host: process.env.MYSQL_HOST,
+  user: process.env.MYSQL_USER,
+  password: process.env.MYSQL_PASSWORD,
+  database: process.env.MYSQL_DB_NAME,
+  port: process.env.MYSQL_PORT,
 
   waitForConnections: true,
   connectionLimit: 10,
